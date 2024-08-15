57ed947f8c34d05853dae4e6c2cd8a4993691a17
Guillermo Olmos <geordev2@gmail.com>
2024-08-15 10:38:44 +0100

    feat: switching from a CDN to make use of tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,15 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const viewsPath = join(
-    __dirname,
-    '..',
-    process.env.NODE_ENV === 'production' ? 'views' : '../views',
-  );
+  const viewsPath = join(__dirname, '..', 'views');
   app.setBaseViewsDir(viewsPath);
   app.setViewEngine('hbs');
 
   app.use(methodOverride('_method'));
   app.use(cookieParser());
+
+  // Serve static files from the 'public' directory
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.hbs', './src/**/*.ts'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
